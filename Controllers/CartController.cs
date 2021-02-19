9d6d39ba12156358f834b34b8012a3a8c7e416c9
ngochien18 <hienotn@gmail.com>
2021-02-19 22:32:16 +0700

    commit

---
+++ Controllers/CartController.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using KH095.Models;
+using KH095.Data;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace KH095.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<CartController> _logger;
+        private ApplicationDbContext db;
+
+        public CartController(ILogger<CartController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            this.db = db;
+        }
+
+
+        // Key lưu chuỗi json của Cart
+        public const string CARTKEY = "cart";
+
+        // Lấy cart từ Session (danh sách CartItem)
+        List<CartItem> GetCartItems()
+        {
+
+            var session = HttpContext.Session;
+            string jsoncart = session.GetString(CARTKEY);
+            if (jsoncart != null)
+            {
+                return JsonConvert.DeserializeObject<List<CartItem>>(jsoncart);
+            }
+            return new List<CartItem>();
+        }
+
+        // Xóa cart khỏi session
+        void ClearCart()
+        {
+            var session = HttpContext.Session;
+            session.Remove(CARTKEY);
+        }
+
+        // Lưu Cart (Danh sách CartItem) vào session
+        void SaveCartSession(List<CartItem> ls)
+        {
+            var session = HttpContext.Session;
+            string jsoncart = JsonConvert.SerializeObject(ls);
+            session.SetString(CARTKEY, jsoncart);
+        }
+
+        [Route("addcart/{id:int}", Name = "addcart")]
+        public IActionResult AddToCart([FromRoute] int id)
+        {
+
+            var product = db.Products
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
+            if (product == null)
+                return NotFound("Không có sản phẩm");
+
+            // Xử lý đưa vào Cart ...
+            var cart = GetCartItems();
+            var cartitem = cart.Find(p => p.product.Id == id);
+            if (cartitem != null)
+            {
+                // Đã tồn tại, tăng thêm 1
+                cartitem.quantity++;
+            }
+            else
+            {
+                //  Thêm mới
+                cart.Add(new CartItem() { quantity = 1, product = product });
+            }
+
+            // Lưu cart vào Session
+            SaveCartSession(cart);
+
+            // Chuyển đến trang hiện thị Cart
+            return RedirectToAction(nameof(Cart));
+        }
+
+        [Route("/cart", Name = "cart")]
+        public IActionResult Cart()
+        {
+            return View(GetCartItems());
+        }
+
+        /// xóa item trong cart
+        [Route("/removecart/{id:int}", Name = "removecart")]
+        public IActionResult RemoveCart([FromRoute] int id)
+        {
+            var cart = GetCartItems();
+            var cartitem = cart.Find(p => p.product.Id == id);
+            if (cartitem != null)
+            {
+                // Đã tồn tại, tăng thêm 1
+                cart.Remove(cartitem);
+            }
+
+            SaveCartSession(cart);
+            return RedirectToAction(nameof(Cart));
+        }
+
+    }
+}
